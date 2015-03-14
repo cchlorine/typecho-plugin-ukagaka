4f53d1f26468de2b60ab3f3854c7b9a2412754ac
Rakume Hayashi <i@fake.moe>
2015-03-14 14:01:22 +0800

    Rewrite Base

--- assets/ukagaka.js
+++ assets/ukagaka.js
@@ -1,3 +1,7 @@
+// Typing.js
+// https://github.com/coffeedeveloper/typing.js
+function Typing(opts){this.version="1.1";this.source=opts.source;this.output=opts.output;this.delay=opts.delay||120;this.chain={parent:null,dom:this.output,val:[]}}Typing.fn=Typing.prototype={toArray:function(eles){var result=[];for(var i=0;i<eles.length;i++){result.push(eles[i])}return result},init:function(){this.chain.val=this.convert(this.source,this.chain.val)},convert:function(dom,arr){var that=this,children=this.toArray(dom.childNodes);children.forEach(function(node){if(node.nodeType===3){arr=arr.concat(node.nodeValue.split(""))}else{if(node.nodeType===1){var val=[];val=that.convert(node,val);arr.push({"dom":node,"val":val})}}});return arr},print:function(dom,val,callback){setTimeout(function(){dom.appendChild(document.createTextNode(val));callback()},this.delay)},play:function(ele){if(!ele){return}if(!ele.val.length&&ele.parent){this.play(ele.parent)}if(!ele.val.length){return}var curr=ele.val.shift();var that=this;if(typeof curr==="string"){this.print(ele.dom,curr,function(){if(ele.val.length){that.play(ele)}else{if(ele.parent){that.play(ele.parent)}}})}else{var dom=document.createElement(curr.dom.nodeName);var attrs=that.toArray(curr.dom.attributes);attrs.forEach(function(attr){dom.setAttribute(attr.name,attr.value)});ele.dom.appendChild(dom);curr.parent=ele;curr.dom=dom;this.play(curr.val.length?curr:curr.parent)}},start:function(){this.init();this.play(this.chain)}};
+
 var Lutachu = function(w, d) {
   if(!w.localStorage || !w.XMLHttpRequest) {
     return alert('请更新浏览器！');
@@ -92,7 +96,7 @@ Ukagaka.prototype = {
     }
 
     this.body.id = 'ukagaka';
-    this.body.innerHTML = '<div class="ukagaka-character"></div><div class="ukagaka-chatlog"></div>';
+    this.body.innerHTML = '<div class="ukagaka-character"></div><div class="ukagaka-chatlog"><div class="ukagaka-chatlog-source"></div><div class="ukagaka-chatlog-output"></div></div>';
     Lutachu('body').appendChild(this.body);
 
     // Set Face
@@ -112,20 +116,49 @@ Ukagaka.prototype = {
       this.body.style.top = this.data['position']['y'] + 'px';
     }
 
+    var self = this;
+    Lutachu.x(this.data.api, function(data) {
+      self.data['born'] = data['born'];
+      self.data['notice'] = data['notice'];
+      self.data['talk'] = [data['question'], data['answer']];
+      self.data['food'] = [data['foods'], data['eatsay']];
+
+      self.show('notice');
+    });
+
+    // Set Event
     this.event();
-    console.log(this.data);
   },
 
-  talk: function(v) {
-    //
+
+  show: function(t) {
+    switch (t) {
+      case 'notice':
+        this.type(this.data['notice']);
+        break;
+
+      case 'menu':
+        break;
+
+      case 'set':
+        break;
+
+      case 'talk':
+        break;
+    }
   },
 
-  set: function(v) {
-    //
-  },
+  type: function(v) {
+    Lutachu('.ukagaka-chatlog-source').innerHTML = v;
+    Lutachu('.ukagaka-chatlog-output').innerHTML = '';
 
-  type: function() {
+    var typing = new Typing({
+      source: Lutachu('.ukagaka-chatlog-source'),
+      output: Lutachu('.ukagaka-chatlog-output'),
+      delay: 20
+    });
 
+    typing.start();
   },
 
   event: function() {
